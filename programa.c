0c458d3d4d1690bd8f017537304cdf3e7de434c8
mathcrap <jairotft@gmail.com>
2016-06-12 21:37:59 -0500

    Mejoras archivo lexico, gitignore, codigo fuente

---
+++ programa.c
@@ -0,0 +1,20 @@
+/*
+COMETARIO
+*/
+// COMETARIO CORTO
+
+int main(){
+    8786767dfgdgfh ;
+    .#sdsdh899u7hjh;
+
+    79j;
+    int a= 5;
+    float l = 5.6;
+    printf("Hola mundo\n");
+    int b = a + a;
+    printf("%d", b);
+inta;
+
+    
+}
+// int
